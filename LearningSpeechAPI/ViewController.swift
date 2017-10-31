1355821fde3d0f5c616d64facfc4ad0996a38278
Rommel Rico <me@rommelrico.com>
2017-10-31 08:42:41 -0700

    Deleting the unused didReceiveMemoryWarning method

--- LearningSpeechAPI/ViewController.swift
+++ LearningSpeechAPI/ViewController.swift
@@ -25,11 +25,6 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     @IBAction func directionsButtonPressed(_ sender: UIButton) {
         if !transcriber.isTranscribing {
             // Not currently listening for directions, so let's start!
