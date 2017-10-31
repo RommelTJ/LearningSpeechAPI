c294698987d8bd6746c7d5bf6e7ffb2046b607a0
Rommel Rico <me@rommelrico.com>
2017-10-31 08:34:21 -0700

    Setting up the UI

--- LearningSpeechAPI/ViewController.swift
+++ LearningSpeechAPI/ViewController.swift
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func directionsButtonPressed(_ sender: UIButton) {
+        // TODO - Implement me.
+    }
+    
 }
 
